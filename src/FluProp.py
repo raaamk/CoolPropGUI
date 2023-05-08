9e107e7df8ce31e637438395874973ebddff6583
markj <mark.jani@web.de>
2023-05-08 03:11:05 +0200

    Code Update

--- src/FluProp.py
+++ src/FluProp.py
@@ -510,6 +510,7 @@ def prop_ausgabe(input1, input2):
 calc_btn = ttk.Button(window, text="Berechnen", command=calc, width=71)
 calc_btn.grid(row=3, column=0, sticky="W", columnspan=3)
 
+
 # Infinite Loop & Fenster schließen
 def on_closing():
     if tkinter.messagebox.askyesno("FluProp schließen", "Möchtest du FluProp beenden?"):
